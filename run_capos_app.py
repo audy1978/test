e22020c44e679fe340b3f8ca4986c9f4f1401487
audy1978 <theelapong.aon@gmail.com>
2026-07-13 09:48:38 +0700

    Add files via upload

---
+++ run_capos_app.py
@@ -0,0 +1,13 @@
+import sys
+
+from streamlit.web import cli as stcli
+
+
+if __name__ == "__main__":
+    sys.argv = [
+        "streamlit",
+        "run",
+        "capos_app.py",
+        "--server.headless=true",
+    ]
+    sys.exit(stcli.main())
